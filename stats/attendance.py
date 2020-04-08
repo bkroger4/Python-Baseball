8f9766966ce5af82a58bf3d57baa5bc5c8ef3976
bkroger4 <60327259+bkroger4@users.noreply.github.com>
2020-04-08 16:59:39 -0600

    part 3.4

--- stats/attendance.py
+++ stats/attendance.py
@@ -11,7 +11,7 @@ attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
 attendance.plot(x='year', y='attendance', figsize=(15,7), kind='bar')
 plt.xlabel('Year')
 plt.ylabel('Attendance')
-plt.axhline(y=attendance['attendance'].mean(), label='Mean', linestyle='--', color='g')
+plt.axhline(y=attendance['attendance'].mean(), label='Mean', linestyle='--', color='green')
 plt.show()
 
 
