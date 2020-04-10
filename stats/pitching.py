827ae934dfe1fe13799ff2cb3d6d984854d3cfed
bkroger4 <60327259+bkroger4@users.noreply.github.com>
2020-04-10 14:46:19 -0600

    pitching

--- stats/pitching.py
+++ stats/pitching.py
@@ -1,2 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from data import games
+
+#plays = games[games['type']=='play']
+
+#strike_outs = plays[plays['event'].str.contains('k')]
+
+#strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+
+
+
+
+
+
+
