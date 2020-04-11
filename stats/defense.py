5266bfbc0f708a1e0925aed8b922c260a26a8bec
bkroger4 <60327259+bkroger4@users.noreply.github.com>
2020-04-10 20:26:11 -0600

    defensive

--- stats/defense.py
+++ stats/defense.py
@@ -1,2 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from frames import games
+from frames import info
+from frames import events
+
+plays = games.query((games['type']=='play')&(games['event']!='NP'))
+
+
+
+
+
+
+
+
+
+
+
+
+
